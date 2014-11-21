d4695063337101d419cb714076cb911ac4b113d0
Nicu Stiurca <nstiurca@seas.upenn.edu>
2014-11-21 16:54:29 -0500

    hack to display local RAM size

--- modules/ocl/src/cl_operations.cpp
+++ modules/ocl/src/cl_operations.cpp
@@ -343,6 +343,10 @@ void openCLVerifyKernel(const Context *ctx, cl_kernel kernel, size_t *localThrea
     size_t kernelWorkGroupSize;
     openCLSafeCall(clGetKernelWorkGroupInfo(kernel, getClDeviceID(ctx),
                                             CL_KERNEL_WORK_GROUP_SIZE, sizeof(size_t), &kernelWorkGroupSize, 0));
+    size_t deviceLocalMemSize;
+    openCLSafeCall(clGetDeviceInfo(getClDeviceID(ctx),
+                                            CL_DEVICE_LOCAL_MEM_SIZE, sizeof(size_t), &deviceLocalMemSize, 0));
+    fprintf(stderr, "device local mem size: %zu\n", deviceLocalMemSize);
     CV_Assert( localThreads[0] <= ctx->getDeviceInfo().maxWorkItemSizes[0] );
     CV_Assert( localThreads[1] <= ctx->getDeviceInfo().maxWorkItemSizes[1] );
     CV_Assert( localThreads[2] <= ctx->getDeviceInfo().maxWorkItemSizes[2] );
